b5badc20b54fc6abbbe0b7554ec2c16b28449ac0
Jamie4224 <jamietrip@hotmail.nl>
2016-11-22 18:17:47 +0100

    Added Short LICENSE to files

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,4 +1,23 @@
 <?php
+/*
+FBPhish
+Copyright (C) 2016  Jamie4224
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+
 
 // This is the configuration of the FB phishing tool!
 // Here you should define all database connection settings and other stuff
